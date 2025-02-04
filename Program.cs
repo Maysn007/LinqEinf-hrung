30ae9e21ba88785aa3ac187005b962655c50fee4
Mathias Junger <MJunger@schueler.hakzell.at>
2025-02-04 12:49:34 +0100

    Projektdateien hinzufügen.

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿var zahlen = new List<int>() { 3, 4 , 1, 6, 11, 8};
+
+// create query
+var nums = from num in zahlen
+           orderby num ascending
+           select num;
+
+zahlen.Add(5);
+
+// access nums
+foreach (var x in nums)
+{
+    Console.WriteLine(x);
+}
+
+var nums2 = from num in zahlen
+           where num % 3 == 0
+           select num;
+
+foreach (var x in nums2)
+{
+    Console.WriteLine(x);
+}
+
+var nums3 = from num in zahlen
+            where num % 2 == 0
+            where num > 2
+            select num;
+
+foreach (var x in nums3) {  Console.WriteLine(x); }
+
+var nums4 = from num in zahlen
+            where num % 2 == 0
+            orderby num ascending
+            select num;
+
+foreach (var x in nums4) { Console.WriteLine(x); }
